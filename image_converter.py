2e995b6bb462b587d8aa77cc617d11b6bcad93bd
smoonmare <smoonmare@gmail.com>
2020-09-08 13:11:50 -0700

    Create image_converter.py
    Rotates the image 90° clockwise
    Resizes the image to 128x128
    Save the image to a new folder in .jpeg format

---
+++ image_converter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+from PIL import Image
+
+old_path = os.path.expanduser('~') + '/images/'
+new_path = os.path.expanduser('~') + '/opt/icons/'
+os.makedirs(new_path)
+
+for image in os.listdir(old_path):
+    if image == ".DS_Store":
+        continue
+    img = Image.open(old_path + image)
+    img.rotate(-90).resize((128, 128)).convert("RGB").save(new_path + image + '.jpeg')
+    img.close()
